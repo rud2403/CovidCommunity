ba3759ff2133668704005d93d09256eebfc2bd80
BGM <qkrrudals2403@naver.com>
2022-03-28 18:24:52 +0900

    feat : 후기 게시판 페이징처리 구현 완료, 게시글 상세정보 작업 중

--- covid-community/src/main/java/com/coco/project/review/ReviewController.java
+++ covid-community/src/main/java/com/coco/project/review/ReviewController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.coco.project.login.LoginDTO;
@@ -11,12 +12,14 @@ import com.coco.project.login.LoginDTO;
 @Controller
 @RequestMapping(value = "/review")
 public class ReviewController {
-
+	
+	// 후기 게시판 - 리스트
 	@GetMapping(value = "/list")
     String list(){
         return "/review/reviewList";
     }
 	
+	// 후기 게시판 - 글쓰기
 	@GetMapping(value = "/write")
 	String write(@AuthenticationPrincipal LoginDTO loginDTO, Model model){
 		
@@ -24,5 +27,22 @@ public class ReviewController {
 		
 		return "/review/reviewWrite";
 	}
+	
+	// 후기 게시판 - 게시글 상세페이지
+//	@GetMapping(value = "/detail")
+//	String detail() {
+//		
+//		return "/review/reviewDetail";
+//	}
+	
+	// 후기 게시판 - 게시글 상세페이지
+	
+	@GetMapping(value = "/detail/{id}") String detail(@PathVariable("id") int
+	boardId, Model model) {
+	
+	model.addAttribute("boardId", boardId);
+	  
+	return "/review/reviewDetail"; }
+	 
 	
 }
